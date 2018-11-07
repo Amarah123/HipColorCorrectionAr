9fd184a3ef1c54d406285b352eadaa0379f01b01
Amarah <1403029@wwstudent.org>
2018-11-07 13:40:32 -0500

    removed unecessary demo projects

--- arcore-android-sdk-master/samples/computervision_c/app/.externalNativeBuild/cmake/debug/arm64-v8a/cmake_install.cmake
+++ arcore-android-sdk-master/samples/computervision_c/app/.externalNativeBuild/cmake/debug/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/rodge/Downloads/arcore-android-sdk-master/arcore-android-sdk-master/samples/computervision_c/app
+# Install script for directory: C:/Users/rodge/HIP/HipColorCorrectionAr/arcore-android-sdk-master/samples/computervision_c/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/rodge/Downloads/arcore-android-sdk-master/arcore-android-sdk-master/samples/computervision_c/app/.externalNativeBuild/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/rodge/HIP/HipColorCorrectionAr/arcore-android-sdk-master/samples/computervision_c/app/.externalNativeBuild/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
